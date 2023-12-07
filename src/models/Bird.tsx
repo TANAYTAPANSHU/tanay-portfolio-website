eaf07fa439d734d9015bbc07f16bba9fd7df0fce
TANAYTAPANSHU <42114467+TANAYTAPANSHU@users.noreply.github.com>
2023-12-07 22:52:03 +0530

    Update Bird.tsx

--- src/models/Bird.tsx
+++ src/models/Bird.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { useAnimations, useGLTF } from "@react-three/drei";
 
-import {birdScene} from "../assets/3d/bird.glb";
+import birdScene from "../assets/3d/bird.glb";
 
 // 3D Model from: https://sketchfab.com/3d-models/phoenix-bird-844ba0cf144a413ea92c779f18912042
 export  default function Bird() {
