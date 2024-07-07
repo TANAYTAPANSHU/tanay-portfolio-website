7ea0347a1323cf266683a5651020f624fa0f9b24
TANAYTAPANSHU <42114467+TANAYTAPANSHU@users.noreply.github.com>
2024-07-07 17:20:24 +0530

    mweb support

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -44,11 +44,11 @@ const Home = () => {
 
   setTimeout(()=>{
    setInstructionVisible(false)
-  },10000 )
+  },5000 )
 
   return (
     <section className="w-full h-screen relative">
-      <div className='absolute top-48 left-0 right-0 z-10 flex items-center justify-center'>
+      <div className='absolute top-24 md:top-48 left-0 right-0 z-10 flex items-center justify-center'>
        {currentStage && <HomeInfo currentStage={currentStage} /> }
      </div>
    {instructionVisible && (
